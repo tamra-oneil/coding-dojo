aed1a8eb8000c2989f1f43d228d3022163703727
NataliiaPodoliaka <nataliia-podoliaka@pluralsight.com>
2018-11-30 18:07:35 +0200

    pair programming

--- katas/caesar-shift-cipher/caesar.js
+++ katas/caesar-shift-cipher/caesar.js
@@ -1,19 +1,24 @@
+const firstSymbol = 'A';
+const lastSymbol = 'Z';
 const caesar = (shift, value) => {
-    const firstSymbol = 'A';
-    const lastSymbol = 'Z';
+
+
     const encrypted = value.split('')
     .map((symbol) => {
         
-        return String.fromCharCode(symbol === ' ' ? symbol.charCodeAt(0) : getNextSymbol(symbol.charCodeAt(0)));
+        return String.fromCharCode(
+            symbol === ' ' ? symbol.charCodeAt(0) : getNextSymbol(symbol.charCodeAt(0), shift));
     })
     .join('');
     console.log('output', encrypted);
     return encrypted;
 };
 
-const getNextSymbol = (currentSymbol) => {
-    //if(symbol.charCodeAt(0) + shift) > char code for Z
-}
+const getNextSymbol = (currentCharCode, shift) => {
+    if((currentCharCode + shift) > lastSymbol.charCodeAt(0)){
+        return String.fromCharCode(firstSymbol.charCodeAt(0));
+    }
+};
 
 module.exports = {
     caesar
